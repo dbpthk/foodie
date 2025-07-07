c630e08b7ba74e87f54d027cc25db7c4e7027f19
Dhruv <dbpthk@gmail.com>
2025-07-07 12:19:52 +1000

    project formating fix

---
+++ src/assets/assets.js
@@ -0,0 +1,19 @@
+import about_background_image from "./about-background-image.png";
+import about_background from "./assets/about-background.png";
+import choose_image from "./choose-image.png";
+import delivery_image from "./delivery-image.png";
+import home_banner_background from "./home-banner-background.png";
+import logo from "./Logo.svg";
+import avtar from "./me.jpg";
+import pick_meals_image from "./pick-meals-image.png";
+
+export const assets = {
+  about_background_image,
+  about_background,
+  choose_image,
+  delivery_image,
+  home_banner_background,
+  logo,
+  avtar,
+  pick_meals_image,
+};
